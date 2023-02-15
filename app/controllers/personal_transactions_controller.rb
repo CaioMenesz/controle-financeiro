7cd1d3b577330644a0e24fd12b48915ff87846b6
root <root@DESKTOP-SRJ5R76>
2023-02-14 23:11:21 -0300

    Controle-financeiro Project

--- app/controllers/personal_transactions_controller.rb
+++ app/controllers/personal_transactions_controller.rb
@@ -2,10 +2,8 @@ class PersonalTransactionsController < ApplicationController
 
   def index
     @personal_transactions = PersonalTransaction.all
-  end
-
-  def sumSalary
-    @sum = PersonalTransaction.contain(title == "Salário").sum(amount)
+    @sum = PersonalTransaction.where(title: "Salário").sum(:amount)
+    @sumDif = PersonalTransaction.where.not(title: "Salário").sum(:amount)
   end
 
 end
