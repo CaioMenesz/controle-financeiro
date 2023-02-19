2263d978d64d70483bbecbef097f39c76b0c606b
Caio Menezes <caiofmen@hotmail.com>
2023-02-19 11:47:27 -0300

    Styling the form transactions

--- app/controllers/personal_transactions_controller.rb
+++ app/controllers/personal_transactions_controller.rb
@@ -12,7 +12,22 @@ class PersonalTransactionsController < ApplicationController
   end
 
   def create 
-    
+    @personal_transaction = PersonalTransaction.new(personal_transaction_params)
+    if @personal_transaction.save
+      redirect_to personal_transactions_path
+    else
+      render :new
+    end
+  end
+
+  def personal_transaction_params
+    params.require(:personal_transaction).permit(
+      :title,
+      :description,
+      :amount,
+      :transaction_type,
+      :transaction_date
+    )
   end
 
 end
